ddd5b963a17dbe26012e37d19933d65ed272bf49
Koh Okada <koh.okada@gmail.com>
2016-03-17 03:24:26 +0900

    画像表示

--- app/src/main/java/kokada/io/jumpman/GameView.java
+++ app/src/main/java/kokada/io/jumpman/GameView.java
@@ -3,5 +3,55 @@ package kokada.io.jumpman;
 /**
  * Created by kokada on 16/03/17.
  */
-public class GameView {
+
+/*
+ * アプリケーションの環境情報とかをグローバル(Android OSの全域）で受け渡しするためのインターフェース
+ アクティビティの起動とかブロードキャスト、インテントの受け取りといった他のアプリからの応答を行え、アンドロイド特有のリソース・クラスにアクセスすることも出来る。
+ */
+import android.content.Context;
+/**
+ * Bitmapクラスはイメージに関するクラスです。外部から画像を読み込んだり、新しく作成したりします。
+ */
+import android.graphics.Bitmap;
+/**
+ * BitmapFactoryクラスは外部ファイルやリソース、ストリームなどからBitmapクラスのオブジェクトを作成するためのクラスです。
+ */
+import android.graphics.BitmapFactory;
+/**
+ *Canvasクラスは、文字、図形等を描画することができます。
+ */
+import android.graphics.Canvas;
+/**
+ * Paintオブジェクトは、描画する場合に使用されるペンのようなものです。
+ ペンのように描画する文字や図形の色や線の太さ等を指定することができます。
+ */
+import android.graphics.Paint;
+/**
+ * Viewクラスは、ビューの土台となる機能を持っているクラスです。
+ */
+import android.view.View;
+
+
+public class GameView extends View {
+    private static final Paint PAINT = new Paint();
+    private Bitmap marioBitmap;
+
+    /**
+     * viewクラス継承
+     * @param context
+     */
+    public GameView(Context context) {
+        super(context);
+
+        marioBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.mario);
+    }
+
+    /**
+     * マリオ画像読み込み
+     * @param canvas
+     */
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(marioBitmap, 0, 0, PAINT);
+    }
 }
--- app/src/main/java/kokada/io/jumpman/MyActivity.java
+++ app/src/main/java/kokada/io/jumpman/MyActivity.java
@@ -10,43 +10,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MyActivity extends AppCompatActivity {
+    private GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        gameView = new GameView(this);
+        setContentView(gameView);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
